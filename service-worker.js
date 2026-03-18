2fb6b486c878b9e8ddb8d8487d7940d54188341e
Kizaru_010 <figmamoon@gmail.com>
2026-03-18 17:41:16 +0500

    Refactor service worker caching logic

--- service-worker.js
+++ service-worker.js
@@ -7,23 +7,14 @@ const urlsToCache = [
   '/favicon.png'
 ];
 
-// Установка кэша
 self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+  event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache)));
 });
 
-// Активация
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
 });
 
-// Перехват запросов
 self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
+  event.respondWith(caches.match(event.request).then(response => response || fetch(event.request)));
 });
